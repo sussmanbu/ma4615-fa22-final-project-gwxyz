2f2fab4330ff188ed5b5fa0b29e47817c93049f4
Duoyifu Xu <dyfxu@bu.edu>
2022-12-01 18:39:14 -0500

    13,14,15

--- static/load_and_clean_data.R
+++ static/load_and_clean_data.R
@@ -17,7 +17,7 @@ nba <- read_csv(here::here("dataset", "nbaNew.csv"),
                                  `2P%` = col_number(),
                                  `eFG%` = col_number(),
                                  `FT%` = col_number()))
-nba_clean<- select(nba, -(blanl), -(blank2), -(`3P`), -(`3PA`),-(`3P%`))%>%
+nba_clean<- select(nba, -(blanl), -(blank2))%>%
   na_if("")%>%
   filter(!row_number() %in% c(24625, 24626, 24627, 24628))
 
@@ -39,10 +39,10 @@ Injury <- Injury %>% filter(!is.na(PlayerName))
 Injury <- Injury %>% mutate(injuries_data = 1)
 Injury$SeasonStart <- substr(Injury$SeasonStart,1,4)
 
-Injury_clean <- Injury %>% filter(SeasonStart %in% c("2014","2015","2016"))
+Injury_clean <- Injury %>% filter(SeasonStart %in% c("2013","2014","2015"))
 
 playoffs <- nba_clean%>%
-  filter(SeasonStart%in%c("2014","2015","2016"))%>%
+  filter(SeasonStart%in%c("2013","2014","2015"))%>%
   filter(Tm%in%c("IND","MIA", "TOT", "CHA", "WAS", "BRK","CHI", "ATL","SAS",
                  "OKC","LAC","HOU","POR","GSW","MEM","DAL", "NOP", "CLE", 
                  "TOR", "BOS", "MIL","DET"))
